5f85a7acc5cd01e55660ea81dda8f20ba1e3cd72
1yasa <xiewendao@gmail.com>
2024-09-06 19:32:33 +0800

    chore: update deps

--- packages/app/utils/env.ts
+++ packages/app/utils/env.ts
@@ -1,3 +1,9 @@
 import { is_prod } from './is'
 
-export const BASE_URL = is_prod ? 'https://if-server.openages.com' : 'http://localhost:8080'
+const { SANDBOX } = process.env
+
+export const BASE_URL = SANDBOX
+	? 'https://if-server-sandbox.openages.com'
+	: is_prod
+		? 'https://if-server.openages.com'
+		: 'http://localhost:8080'
