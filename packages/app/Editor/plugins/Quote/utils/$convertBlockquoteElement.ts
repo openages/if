015e06d9f2b8dac08f40fbbf0b2bbc2cec52d696
1yasa <xiewendao@gmail.com>
2024-10-29 20:16:03 +0800

    fix(List): batch import number list value incorrect

--- packages/app/Editor/plugins/Quote/utils/$convertBlockquoteElement.ts
+++ packages/app/Editor/plugins/Quote/utils/$convertBlockquoteElement.ts
@@ -1,9 +1,13 @@
+import { setNodeIndentFromDOM } from 'lexical'
+
 import { $createQuoteNode } from './index'
 
 import type { ElementFormatType } from 'lexical'
 
 export default (el: HTMLElement) => {
 	const node = $createQuoteNode()
+
+	setNodeIndentFromDOM(el, node)
 
 	if (el?.style?.textAlign) node.setFormat(el.style.textAlign as ElementFormatType)
 
--- packages/app/runtime/bootstrap.tsx
+++ packages/app/runtime/bootstrap.tsx
@@ -4,6 +4,20 @@ import '@/presets'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 
+import { is_dev } from '@/utils'
+
 import router from './router'
 
 createRoot(document.getElementById('root')!).render(<RouterProvider router={router}></RouterProvider>)
+
+if (is_dev) {
+	const root_el = document.getElementById('root')
+
+	setTimeout(() => {
+		const iframe = root_el?.nextElementSibling as HTMLIFrameElement
+
+		if (iframe?.tagName === 'IFRAME') {
+			iframe.remove()
+		}
+	}, 30)
+}
