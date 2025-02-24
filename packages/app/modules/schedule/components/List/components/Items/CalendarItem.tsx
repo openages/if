87ab02b7c58e7b1931b8c37595de8ec22383c528
1yasa <xiewendao@gmail.com>
2025-02-24 13:23:46 +0800

    improve(Schedule): timeblock list add cross time progress

--- packages/app/modules/schedule/components/List/components/Items/CalendarItem.tsx
+++ packages/app/modules/schedule/components/List/components/Items/CalendarItem.tsx
@@ -1,4 +1,5 @@
 import { useMemoizedFn } from 'ahooks'
+import { Progress } from 'antd'
 import dayjs from 'dayjs'
 import { useMemo } from 'react'
 
@@ -15,12 +16,15 @@ const Index = (props: IPropsListItem) => {
 	const { start_time, end_time } = item
 	const tag_styles = useTagStyles(tags, item.tag)
 
-	const duration = useMemo(() => {
+	const { cross_time, duration } = useMemo(() => {
 		const start = dayjs(start_time)
 		const end = dayjs(end_time)
 		const cross_time = getCrossTime(start, end)
 
-		return `${cross_time} ${start.format('YYYY-MM-DD HH:mm')} - ${end.format('HH:mm')}`
+		return {
+			cross_time,
+			duration: `${start.format('MM-DD HH:mm')} - ${end.format('HH:mm')}`
+		}
 	}, [start_time, end_time])
 
 	const onJump = useMemoizedFn(() => jump(dayjs(start_time)))
@@ -37,7 +41,14 @@ const Index = (props: IPropsListItem) => {
 			<span className='tag_mark'></span>
 			<div className='flex flex_column'>
 				<span className='text line_clamp_1'>{item.text ?? '---'}</span>
-				<span className='duration'>{duration}</span>
+				<div className='cross_wrap flex align_center'>
+					<span className='duration'>{duration}</span>
+					<span className='cross_time'>{cross_time}</span>
+					<span
+						className='hours_wrap'
+						style={{ '--percent': parseFloat(cross_time.replace('h', '')) / 1 }}
+					></span>
+				</div>
 			</div>
 			<div className='btn_jump none justify_center align_center absolute clickable' onClick={onJump}>
 				<ArrowRight></ArrowRight>
